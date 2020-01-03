0265810f9a82d622881f05b75d66888c291c39b3
Nicolas SIMON <nsimon@student.42.fr>
2020-01-03 22:58:58 +0100

    convert hex

--- srcs/ft_printf_pointer.c
+++ srcs/ft_printf_pointer.c
@@ -6,16 +6,34 @@
 /*   By: nsimon <nsimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/03 21:34:23 by nsimon            #+#    #+#             */
-/*   Updated: 2020/01/03 21:59:30 by nsimon           ###   ########.fr       */
+/*   Updated: 2020/01/03 22:55:52 by nsimon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <inttypes.h>
 
+char	*convert_hex(long value)
+{
+	int		i;
+	int		val;
+	char	actuel[2];
+	char	*conv;
+
+	i = 0;
+	conv = NULL;
+	actuel[1] = '\0';
+	while (value > 0)
+	{
+		val = (int)(value % 16);
+		actuel[0] = val + '0';
+		conv = ft_strjoin(conv, actuel)
+	}
+}
+
 void	ft_pointer(void *ptr, t_fill_list *fill)
 {
-	long long test = (uintptr_t)ptr;
+	long test = (uintptr_t)ptr;
 
 	ft_putstr_fd("0x", 1);
 	printf("\npointeur l : %lld\n pointeur p : %p", test, ptr);
